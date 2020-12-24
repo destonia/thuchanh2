302fc96f40d4c3b1583f8cb4acf45519132f53ff
destonia <lanhthanh.dktd@gmail.com>
2020-12-24 11:55:09 +0700

    thực hành

---
+++ suDungToanTu/JS.js
@@ -0,0 +1,8 @@
+let ipheight;
+let ipwidth;
+ipheight=prompt("Nhập chiều cao");
+ipwidth=prompt("Nhập chiều rộng");
+let height=parseInt(ipheight);
+let width=parseInt(ipwidth);
+let area=height*width;
+document.write("diện tích hình chữ nhật "+" chiều rộng: "+width+" chiều cao: "+height+" là: "+ area);
